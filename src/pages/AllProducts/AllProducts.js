d779ecb2316ac62507ef77e7c07554014640c207
mursona <mursona>
2022-11-28 18:32:33 +0600

    card issue fixed

--- src/pages/AllProducts/AllProducts.js
+++ src/pages/AllProducts/AllProducts.js
@@ -29,8 +29,29 @@ const AllProducts = () => {
      
 
     return (
-        <div className='my-10'>
-            <h2 className='text-center text-3xl text-gray-800'> {category_name} Brand products</h2>
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <h2 className="max-w-lg mb-5 font-sans text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none  leading-none md:mx-auto">
+            <span className="relative inline-block"> Category of
+                    <span className="relative text-pink-600"> {category_name} </span>
+                    <svg
+                        viewBox="0 0 52 24"
+                        fill="currentColor"
+                        className="absolute top-0 -right-32 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                    >
+                        <defs>
+                        <pattern id="bfcc89c7-3b4a-491a-bc7e-53e26502ff69" x="0" y="0" width=".135" height=".30">
+                        <circle cx="1" cy="1" r=".7" />
+                        </pattern>
+                        </defs>
+                        <rect
+                        fill="url(#bfcc89c7-3b4a-491a-bc7e-53e26502ff69)"
+                        width="50"
+                        height="22"
+                        />
+                    </svg>
+                    </span>
+            </h2>
+            <div className='grid gap-8 mt-10 row-gap-8 lg:grid-cols-2'>
             {
               product.length ?  product.map(prod => <ProductCard setmodalinfo={setmodalinfo} key = {prod._id} prod = {prod}></ProductCard>)
               :
@@ -41,7 +62,7 @@ const AllProducts = () => {
               </div>
             }
             { modalinfo && <BookModal setmodalinfo={setmodalinfo} modalinfo={modalinfo}></BookModal>}
-            
+            </div>
         </div>
     );
 };
--- src/pages/AllProducts/ProductCard.js
+++ src/pages/AllProducts/ProductCard.js
@@ -2,6 +2,15 @@ import React from "react";
 import { FiPhoneCall } from 'react-icons/fi';
 import { ImLocation2 } from 'react-icons/im';
 import { useQuery } from "@tanstack/react-query";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+import { FaAddressCard } from "react-icons/fa";
+
 const ProductCard = ({ prod ,setmodalinfo}) => {
   const {
     product_name,
@@ -36,36 +45,59 @@ const ProductCard = ({ prod ,setmodalinfo}) => {
 
 
   return (
-    <div className="containerdiv border pl-5 my-5 flex flex-col justify-start w-11/12 mx-auto md:justify-evently md:flex-row gap-10 ">
-      <div className="left_side lg:w-[30%] md:w-[30%] m-5 md:m-0 lg:m-0">
-        <div className="img_div mt-5">
-          {" "}
-          <img className="w-full rounded-md shadow-md" src={image} alt="" />{" "}
-        </div>
-        <div className="flex items-center justify-between py-2">
-        <h2 className="text-3xl text-orange-300 font-bold"> $ {product_price}</h2>
-        <span className="text-xl text-orange-300">{brand_name}</span>
-        </div>
-      </div>
-      <div className="right_side p-5 lg:w-[-60%] md:w-[60%]">
-        <h2 className="text-4xl mb-3 text-lime-800 font-bold"> {product_name} </h2>
-        <div className="flex items-center justify-between mb-3">
-        <div className="flex items-center"> <span className="text-2xl font-semi-bold mr-2">Post by - {sellerName}</span>
-           { user?.verified === true && <span className="text-xl text-blue-500"><ImLocation2/></span>}
+      <Card className="w-full bg-pink-50">
+        <div className="grid gap-2 row-gap-2 lg:grid-cols-2">
+          <div>
+          <CardHeader color="blue" className="relative h-56 text-center">
+          <img
+          src={image}
+          alt="img-blur-shadow"
+          className="h-full w-full"/>
+          </CardHeader>
           </div>
-        <small className="text-orange-300">{time} </small>
+          <div>
+          <CardBody>
+      <Typography variant="large" className='font-semibold'>{product_name}</Typography>
+      <Typography variant="small" color="gray" className="flex gap-1">
+          <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
+          Category of {brand_name} 
+        </Typography>
+        <Typography variant="h5" className="mb-2">
+        $ {product_price}
+        </Typography>
+        <Typography variant="small" color="gray" className="mr-2">
+        Market price {Market_Price}
+        </Typography>
+        <Typography variant="small" color="gray" className="mr-2">
+        purchase year {purchase_year}
+        </Typography>
+        <Typography variant="small" color="gray" className="mr-2">
+        condition {condition_type}
+        </Typography>
+        <Typography className='mx-4'>
+        {productDetails.slice(0,300)}
+        </Typography>
+      </CardBody>
+          </div>
         </div>
-        <p className="description"> {productDetails.slice(0,300)} </p>
-        <div className=" flex flex-wrap my-2 rounded-md"> 
-          <span className="flex mx-1 p-1 bg-[#edeeed] my-1 rounded-md items-center"> <div className="mr-2"><FiPhoneCall/></div> {mobile} </span>
-          <span className="flex mx-1 p-1 bg-[#edeeed] my-1 rounded-md items-center"> Market price {Market_Price}, </span>
-          <span className="flex mx-1 p-1 bg-[#edeeed] my-1 rounded-md items-center"> purchase year {purchase_year}, </span>
-          <span className="flex mx-1 p-1 bg-[#edeeed] my-1 rounded-md items-center">  condition {condition_type}, </span>
-          <span className="flex mx-1 p-1 bg-[#edeeed] my-1 rounded-md items-center"> <ImLocation2/> {location} </span>
-        </div>
-        <div> <label onClick={()=>setmodalinfo(prod)} htmlFor="openmodal" className="btn btn-success">Book Now</label> </div>
-      </div>
-    </div>
+        <div className="flex mx-4">
+          <Typography variant="small">Post by - {sellerName}</Typography>
+          <Typography>{ user?.verified === true && <span className="text-xl text-green-600 mx-6"><FaAddressCard></FaAddressCard></span>}</Typography>
+      <Typography variant="small">{time}</Typography>
+          </div>
+        <hr />
+        <CardFooter divider className="flex items-center justify-between py-3">
+        <Typography variant="small" color="gray" className="flex gap-1 mr-2">
+        <div className="mr-2"><FiPhoneCall/></div> {mobile}
+        </Typography>
+        <Typography variant="small" color="gray" className="mr-2">
+        <ImLocation2/> {location}
+        </Typography>
+        <Typography variant="small" color="gray" className="mr-2">
+        <label onClick={()=>setmodalinfo(prod)} htmlFor="openmodal" className="btn btn-success">Book Now</label>
+        </Typography>
+      </CardFooter>
+      </Card>
   );
 };
 
